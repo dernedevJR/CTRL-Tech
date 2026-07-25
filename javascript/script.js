b2b376114ed5a972ed3d0cca5516a7b748c8688f
Derne Jr <devdernejr@gmail.com>
2026-07-25 17:58:49 -0300

    adiciona validacao parcial

---
+++ javascript/script.js
@@ -0,0 +1,23 @@
+const form = document.querySelector('.formulary');
+const nome = document.querySelector('.name');
+
+form.addEventListener("submit", function (e) {
+
+    valor = nome.value.trim();
+    mensagem = document.getElementById('elementoJs')
+
+    if (valor === '') {
+        mensagem.classList.add('erro');
+        mensagem.innerText = '⚠ Preencha o campo com o seu nome';
+
+    }
+    else {
+        
+        mensagem.innerText = 'Sucesso!';
+    }
+
+    e.preventDefault();
+});
+nome.addEventListener('input', () => {
+    mensagem.innerText = ''
+})
