4a19af6c38f87611113042b1f361e91214944c01
Derne Jr <devdernejr@gmail.com>
2026-08-04 08:02:52 -0300

    teste de comunicacao com o server

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,20 @@
-const express = required("express");
-const server = express();
+// Carregar o módulo, que por sua vez retorna uma função.
+const express = require('express');
+// Recebe a função que vem do módulo 'express'.
+const app = express();
+
+// Rotas (caminhos)
+app.get("/", (req,res)=>{
+    res.send("Olá mundo!");
+});
+
+
+
+
+
+
+// Coloca o site para rodar 
+app.listen(3000, () => {
+    console.log('Servidor rodando na url http://localhost:3000')
+})
+//localhost:3000
