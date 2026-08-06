4853fdcbeb4716b92679d8a88deed157d74f5f87
Derne Jr <devdernejr@gmail.com>
2026-08-05 22:04:06 -0300

    'backend'

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,13 @@ const express = require('express');
 // Recebe a função que vem do módulo 'express'.
 const app = express();
 
-// Rotas (caminhos)
-app.get("/", (req,res)=>{
-    res.send("Olá mundo!");
+// Rota POST 
+
+app.post("/enviar-dados", (req , res) => {
+    res.send("Olá mundo!!")
 });
 
-
-
-
-
-
-// Coloca o site para rodar 
+// Motorzinho 
 app.listen(3000, () => {
     console.log('Servidor rodando na url http://localhost:3000')
 })
-//localhost:3000
